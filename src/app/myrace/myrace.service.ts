42d455d74590f11bc20d5dc6a707e46389cb7e73
Dimitrios Chondrokoukis <dimitrios1988@msn.com>
2026-01-22 14:32:42 +0200

    Added error handling in getRunnerInfo to logout and redirect on failure

--- src/app/myrace/myrace.service.ts
+++ src/app/myrace/myrace.service.ts
@@ -1,9 +1,11 @@
-import { effect, inject, Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { IRunner } from './runner.interface';
 import { AUTH_CREDENTIALS } from '../secrets';
-import { map, Observable, tap } from 'rxjs';
+import { catchError, map, Observable, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { RunnerInfoResponse } from './runner.info.resp';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -12,15 +14,15 @@ export class MyRaceService {
   private readonly _runner = signal<IRunner | null>(null);
   public readonly runner$ = this._runner.asReadonly();
   private readonly http: HttpClient = inject(HttpClient);
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
 
-  constructor() {
-    effect(() => {});
-  }
+  constructor() {}
 
   getRunnerInfo(uuid: string): Observable<IRunner> {
     const runnerInfoApi = new URL(
       `/api/mobile_app_manager/get_runner_info/v1`,
-      AUTH_CREDENTIALS.app_url
+      AUTH_CREDENTIALS.app_url,
     ).toString();
     // Implementation of the HTTP request to fetch runner info goes here
     return this.http
@@ -32,6 +34,7 @@ export class MyRaceService {
         map((response: RunnerInfoResponse[]) => {
           return {
             runner: {
+              uuid: response[0]['0(runner)'].uuid,
               bib: response[0]['0(runner)'].bib,
               birthdate: response[0]['0(runner)'].birthdate
                 ? new Date(response[0]['0(runner)'].birthdate * 1000)
@@ -57,7 +60,12 @@ export class MyRaceService {
         }),
         tap((runner: IRunner) => {
           this._runner.set(runner);
-        })
+        }),
+        catchError((error) => {
+          this.authService.logoutRunner();
+          this.router.navigate(['/tabs/login']);
+          throw error;
+        }),
       );
   }
 }
