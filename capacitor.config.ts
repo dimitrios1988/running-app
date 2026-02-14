35c8819915deaf63148b89b416436958bcfc6941
Dimitrios Chondrokoukis <dimitrios1988@msn.com>
2026-02-14 12:09:16 +0200

    Added splash screen, logo and changed package manager to cocoapods.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'running-app',
+  appName: 'Running App',
   webDir: 'www',
   ios: {
     handleApplicationNotifications: false,
